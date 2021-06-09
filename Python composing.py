bdcff5e975d98b86ce17bf121e734720786eae1c
jrussell86 <80422198+jrussell86@users.noreply.github.com>
2021-06-09 18:35:47 -0400

    Add files via upload

---
+++ Python composing.py
@@ -0,0 +1,8 @@
+#array with note names
+#array with lengths/rhythms
+
+notes = #random from note array plus random value 0-4
+lengths = #random from lengths array
+comp = notes + lengths
+
+#run four times and print output at the end
